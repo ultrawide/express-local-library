e576633510e69472520f532a36703117064e5c52
ultrawide <colinchan02@gmail.com>
2020-04-25 17:18:28 -0700

    Add routing comments

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.redirect('/catalog');
+  res.redirect('/catalog'); // Re-route base homepage to catalog
 });
 
 module.exports = router;
