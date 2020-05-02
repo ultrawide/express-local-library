129b173febf06e0d89e573caaaf6ed38692134aa
ultrawide <colinchan02@gmail.com>
2020-05-02 13:51:40 -0700

    Add helmet to middleware chain to protect against well known vulnerabilities

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var compression = require('compression');
-
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
 
 var app = express();
+
+// Protect against well known vulnerabilities
+app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
