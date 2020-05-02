14a848195fc4584d73cf8d8f10b7abd1e7a7b882
ultrawide <colinchan02@gmail.com>
2020-05-02 14:06:08 -0700

    Add node version and setup for Heroku deployment

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://ultrawide:dummy123@locallibrarycluster-t1nkn.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://ultrawide:dummy123@locallibrarycluster-t1nkn.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
